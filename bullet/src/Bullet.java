d3adbcb20b759379508e6924764cebd5c263b045
Albert Liu <liualbert99@gmail.com>
2017-05-26 09:55:09 -0400

    Added Bullet class structure

--- bullet/src/Bullet.java
+++ bullet/src/Bullet.java
@@ -1,8 +1,30 @@
 
 public abstract class Bullet {
 
-    private int xPosition;
-    private int yPosition;
+	private int xPosition;
+	private int yPosition;
 
+	public Bullet(int xInitial, int yInitial) {
+		this.xPosition = xInitial;
+		this.yPosition = yInitial;
+	}
+
+	public void setXPosition(int x) {
+		this.xPosition = x;
+	}
+
+	public void setYPosition(int y) {
+		this.yPosition = y;
+	}
+	
+	public int getXPosition() {
+		return this.xPosition;
+	}
+
+	public int getYPosition() {
+		return this.yPosition;
+	}
+
+	public abstract void bulletChangeEquation();
 
 }
