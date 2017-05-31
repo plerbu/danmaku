88d7c5cffec9b48f846edc893f21e5a6ddda23fc
Albert Liu <liualbert99@gmail.com>
2017-05-31 10:06:35 -0400

    Added basic GUI

---
+++ danmaku/src/Application.java
@@ -0,0 +1,35 @@
+/**
+ * Created by blerpu on 5/31/17.
+ */
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+public class Application extends JFrame {
+
+    public Application() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        setSize(250,200);
+
+        setTitle("Predator Quest");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Application ex = new Application();
+                ex.setVisible(true);
+            }
+        });
+    }
+
+}
---
+++ danmaku/src/Board.java
@@ -0,0 +1,12 @@
+/**
+ * Created by blerpu on 5/31/17.
+ */
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel{
+
+    public Board() {}
+
+
+}
